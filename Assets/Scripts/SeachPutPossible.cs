c9e56cc985f48c04bf3d82aeaef5ffce090d8b08
Kitagawa20227017 <149038762+Kitagawa20227017@users.noreply.github.com>
2024-04-03 14:49:37 +0900

    2024/4/3

--- Assets/Scripts/SeachPutPossible.cs
+++ Assets/Scripts/SeachPutPossible.cs
@@ -3,8 +3,8 @@
 //
 // 設置可能マスを調べる  
 //
-// 作成日:  
-// 作成者:  
+// 作成日: 2024/4/1
+// 作成者: 北川 稔明
 // ---------------------------------------------------------  
 using UnityEngine;
 
@@ -27,22 +27,32 @@ public class SeachPutPossible : MonoBehaviour
     // 黒の石の値
     private const int BLACK_STONE_INDEX = 1;
 
+    // 盤面に置ける最大石数
+    private const int STONE_MAX_SUM = 64;
+
     #endregion
 
-    [SerializeField]
+    [SerializeField,Header("GameManagerオブジェクト")]
     private GameObject _gameManager = default;
 
+    // GameManagerスクリプト格納用
     private GameManager _gameManagerScript = default;
 
-    private Transform _transform = default;
+    // Transform取得用
+    private Transform _parentTransform = default;
+    
+    // 盤面情報
     private int[,] _surfacePlate = new int[8, 8];
 
+    // それぞれの石の置ける位置情報
     private int[,] _whiteSurfacePlate = new int[8, 8];
     private int[,] _blackSurfacePlate = new int[8, 8];
 
+    // それぞれの石の置ける数 
     private int _whiteStoneConut = 0;
     private int _blackStoneConut = 0;
 
+    //  それぞれの石の合計
     private int _whiteStoneSum = 0;
     private int _blackStoneSum = 0;
 
@@ -74,23 +84,28 @@ public class SeachPutPossible : MonoBehaviour
     /// </summary>  
     private void Start ()
     {
-        _transform = this.transform;
+        // 初期設定
+        _parentTransform = this.transform;
         _gameManagerScript = _gameManager.GetComponent<GameManager>();
         Seach();
     }
 
+    /// <summary>
+    /// 探索処理
+    /// </summary>
     public void Seach()
     {
-
-        if (_transform.childCount >= 64)
+        // 石が盤面全てに埋まっていたら終了
+        if (_parentTransform.childCount >= STONE_MAX_SUM)
         {
+            // それぞれの石が何個か数える
             foreach(int stone in _surfacePlate)
             {
-                if(stone == 1)
+                if(stone == BLACK_STONE_INDEX)
                 {
                     _blackStoneSum++;
                 }
-                else if(stone == -1)
+                else if(stone == WHITE_STONE_INDEX)
                 {
                     _whiteStoneSum++;
                 }
@@ -127,7 +142,7 @@ public class SeachPutPossible : MonoBehaviour
         }
 
         // 子オブジェクト探索
-        foreach (Transform chlid in _transform)
+        foreach (Transform chlid in _parentTransform)
         {
             // 座標をintにする
             int verticalAxis = (int)chlid.localPosition.x;
@@ -182,13 +197,13 @@ public class SeachPutPossible : MonoBehaviour
                     Seach2(i, j);
                 }
 
-                // 現在のマスが範囲内で石が置いてなく下隣に石があるとき
+                // 現在のマスが範囲内で石が置いてなく下に石があるとき
                 if (i != _surfacePlate.GetLength(0) - 1 && _surfacePlate[i + 1, j] != 0 && _surfacePlate[i, j] == 0)
                 {
                     Seach3(i, j);
                 }
 
-                // 現在のマスが範囲内で石が置いてなく上隣に石があるとき
+                // 現在のマスが範囲内で石が置いてなく上に石があるとき
                 if (i != 0 && _surfacePlate[i - 1, j] != 0 && _surfacePlate[i, j] == 0)
                 {
                     Seach4(i, j);
@@ -230,179 +245,281 @@ public class SeachPutPossible : MonoBehaviour
             }
         }
 
+        // 白の置ける場所がないかつ白のターンのとき
         if (_whiteStoneConut == 0 && !_gameManagerScript.IsTurn)
         {
             _gameManagerScript.aa();
         }
-        else if(_blackStoneConut == 0 && _gameManagerScript.IsTurn)
+        // 黒の置ける場所がないかつ黒のターンのとき
+        else if (_blackStoneConut == 0 && _gameManagerScript.IsTurn)
         {
             _gameManagerScript.aa();
         }
     }
 
+    /// <summary>
+    /// 右隣の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach1(int x,int z)
     {
-        int n = _surfacePlate[x, z +1];
+        // 右隣の色を見る
+        int coler = _surfacePlate[x, z +1];
+
+        // 色を見た隣の色を見ていく
         for(int i = z + 2; i < _surfacePlate.GetLength(1); i++)
         {
+            // 何もなかったとき
             if(_surfacePlate[x,i] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[x, i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x, i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 左隣の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach2(int x, int z)
     {
-        int n = _surfacePlate[x, z - 1];
+        // 左隣の色を見る
+        int coler = _surfacePlate[x, z - 1];
+
+        // 色を見た隣の色を見ていく
         for (int i = z - 2; i > 0; i--)
         {
+            // 何もなかったとき
             if (_surfacePlate[x, i] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[x, i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x, i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 下の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach3(int x, int z)
     {
-        int n = _surfacePlate[x + 1, z];
+        // 下の色を見る
+        int coler = _surfacePlate[x + 1, z];
+
+        // 色を見た隣の色を見ていく
         for (int i = x + 2; i < _surfacePlate.GetLength(0); i++)
         {
+            // 何もなかったとき
             if (_surfacePlate[i, z] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[i, z] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[i, z] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 上の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach4(int x, int z)
     {
-        int n = _surfacePlate[x - 1, z];
+        // 上の色を見る
+        int coler = _surfacePlate[x - 1, z];
+
+        // 色を見た隣の色を見ていく
         for (int i = x - 2; i > 0; i++)
         {
+            // 何もなかったとき
             if (_surfacePlate[i, z] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[i, z] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[i, z] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 左下の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach5(int x, int z)
     {
-        int n = _surfacePlate[x + 1, z + 1];
+        // 左下の色を見る
+        int coler = _surfacePlate[x + 1, z + 1];
+
+        // 色を見た隣の色を見ていく
         for (int i = 2; i < _surfacePlate.GetLength(0); i++)
         {
+            // 範囲外なら探索をやめる
             if(x + i >= _surfacePlate.GetLength(0) || z + i >= _surfacePlate.GetLength(1))
             {
                 break;
             }
 
-            if(_surfacePlate[x + i,z + i] == 0)
+            // 何もなかったとき
+            if (_surfacePlate[x + i,z + i] == 0)
             {
                 break;
             }
-            else if(_surfacePlate[x + i, z + i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x + i, z + i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 右上の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach6(int x, int z)
     {
-        int n = _surfacePlate[x - 1, z + 1];
+        // 右上の色を見る
+        int coler = _surfacePlate[x - 1, z + 1];
+
+        // 色を見た隣の色を見ていく
         for (int i = 2; i < _surfacePlate.GetLength(0); i++)
         {
+            // 範囲外なら探索をやめる
             if (x - i < 0 || z + i >= _surfacePlate.GetLength(1))
             {
                 break;
             }
 
+            // 何もなかったとき
             if (_surfacePlate[x - i, z + i] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[x - i, z + i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x - i, z + i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 右下の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach7(int x, int z)
     {
-        int n = _surfacePlate[x + 1, z - 1];
+        // 右下の色を見る
+        int coler = _surfacePlate[x + 1, z - 1];
+
+        // 色を見た隣の色を見ていく
         for (int i = 2; i < _surfacePlate.GetLength(0); i++)
         {
+            // 範囲外なら探索をやめる
             if (x + i >= _surfacePlate.GetLength(0) || z - i < 0)
             {
                 break;
             }
 
+            // 何もなかったとき
             if (_surfacePlate[x + i, z - i] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[x + i, z - i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x + i, z - i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// 左下の探索
+    /// </summary>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
     private void Seach8(int x, int z)
     {
-        int n = _surfacePlate[x - 1, z - 1];
+        // 左下の色を見る
+        int coler = _surfacePlate[x - 1, z - 1];
+
+        // 色を見た隣の色を見ていく
         for (int i = 2; i < _surfacePlate.GetLength(0); i++)
         {
+            // 範囲外なら探索をやめる
             if (x - i < 0 || z - i < 0)
             {
                 break;
             }
 
+            // 何もなかったとき
             if (_surfacePlate[x - i, z - i] == 0)
             {
                 break;
             }
-            else if (_surfacePlate[x - i, z - i] == -n)
+            // 別の色(白なら黒、黒なら白)があったとき
+            else if (_surfacePlate[x - i, z - i] == -coler)
             {
-                aaa(n, x, z);
+                // 位置と色を渡す
+                PutStoneColer(coler, x, z);
                 break;
             }
         }
     }
 
-    private void aaa(int n,int x,int z)
+    /// <summary>
+    /// 置ける色の判定
+    /// </summary>
+    /// <param name="coler">石の色</param>
+    /// <param name="x">縦軸</param>
+    /// <param name="z">横軸</param>
+    private void PutStoneColer(int coler,int x,int z)
     {
-        if(n == 1)
+        //  
+        if(coler == BLACK_STONE_INDEX)
         {
             _blackSurfacePlate[x, z] = 1;
         }
-        else if(n == -1)
+        //
+        else if(coler == WHITE_STONE_INDEX)
         {
             _whiteSurfacePlate[x, z] = 1;
         }
